26a94cbc852b595ac1ccab081210115ed40f19a8
Henrik Nielsen <nielsen.henrik@gmail.com>
2011-08-25 01:34:39 +0200

    Inital commit

---
+++ Gemfile
@@ -0,0 +1,25 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.rc5'
+
+# Bundle edge Rails instead:
+# gem 'rails',     :git => 'git://github.com/rails/rails.git'
+
+gem 'sqlite3'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails', "~> 3.1.0.rc"
+  gem 'coffee-rails', "~> 3.1.0.rc"
+  gem 'uglifier'
+end
+
+gem 'jquery-rails'
+
+gem "rspec-rails", ">= 2.6.1", :group => [:development, :test]
+
+group :test do
+  gem 'webrat'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailstutChap3::Application.config.secret_token = '55b4b778df29b2385251999a90a56d46ed65c7e300b2d1b722d7d7a3965ac531b80af0e04f0c4d221818496e07be9c8d1d39007c845829b32415cc4f17cf2550'
