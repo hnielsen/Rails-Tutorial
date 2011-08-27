bb8149810fa0c117ea04d815de119a9ee58016a2
Henrik Nielsen <nielsen.henrik@gmail.com>
2011-08-28 00:47:30 +0200

    Completed chapter 5 excercises.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,10 @@
 module ApplicationHelper
 
+  # Creates an image_tag for the site logo
+  def logo
+    image_tag("logo.png", :alt => "sample app", :class => "round")
+  end
+
   # Set page title on a per-page basis
   def title
     base_title = "Ruby on Rails Tutorial Sample App"
