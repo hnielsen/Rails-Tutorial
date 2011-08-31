b862091957486090b008203228b8240fd805847c
Henrik Nielsen <nielsen.henrik@gmail.com>
2011-09-01 01:31:21 +0200

    Completed 8.2.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,23 @@ class UsersController < ApplicationController
   end
 
   def new
+    @user = User.new
     @title = "Sign up"
   end
 
+  def create
+    @user = User.new(params[:user])
+  
+    respond_to do |wants|
+      if @user.save
+        flash[:notice] = 'User was successfully created.'
+        wants.html { redirect_to(@user) }
+        wants.xml  { render :xml => @user, :status => :created, :location => @user }
+      else
+        @title = "Sign up"
+        wants.html { render :action => "new" }
+        wants.xml  { render :xml => @user.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
 end
